37d3bd6b5367e5001c828a5a33d78f85b61002ca
lenglengai <lenglengai@gmail.com>
2014-03-17 17:55:18 +0800

    update

---
+++ csharp/20140222/com.core/Closed/Count/ICount.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace com.core
+{
+    public interface ICount
+    {
+        void runCount(int nClassify, int nOpened, IList<object> nOpeneds);
+
+        string getCountName();
+    }
+}
